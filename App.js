798f33e6452593bd7a8180e135f5697ed39e60fb
Ardalber <[email-du-dveloppeur]cochezgaetan07@gmail.com>
2021-01-26 17:04:07 +0100

    a

--- App.js
+++ App.js
@@ -1,30 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import axios from 'axios';
+import HeaderToday from './components/HeaderToday';
 
 export default function App() {
-    useEffect(() => {
-      axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=50.4291723&lon=2.8319805&units=metric&lang=fr&exclude=minutely,hourly,alerts&appid=8c3a54c385c9c9d874d88f2cd6b3dda8')
-      .then(res => {
-      (res.data)
-    })
-  }, [])
   var today = new Date();
   return (
     <View style={styles.container}>
-      <Text>{today.getDate()}-{today.getMonth()+1}-{today.getFullYear()}</Text>
-      <StatusBar style="auto" />
-      
+      <HeaderToday></HeaderToday>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
